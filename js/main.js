9b1a8af1c219d7a83b0a0dff4b9e0f6b730f274b
Fabio Moro <fabio.moro2000@gmail.com>
2022-04-27 13:01:01 +0200

    aggiunta prima versione

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const firstName = prompt("scrivi il tuo nome!");
+const lastName = prompt("scrivi il tuo cognome!");
+const color = prompt("qual'è il tuo colore preferito?");
+
+document.getElementById("password").innerHTML = firstName + lastName + color + 21;
